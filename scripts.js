96bcf78e239550f547e5f98808dd9e28d3b0bb27
Lamiel01 <83187094+Lamiel01@users.noreply.github.com>
2021-05-15 19:41:12 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,34 +1,27 @@
-// Functions
-
 // Listeners
 document.addEventListener('DOMContentLoaded', function () {
     const item = document.getElementById('add');
-    const read = document.getElementById('read');
-
-    // const stack = document.getElementById('flex-stack');
-
     const addButton = document.getElementById('add-queue');
-    const readButton = document.getElementById('read-queue');
     const removeButton = document.getElementById('remove-queue');
 
     addButton.onclick = addQueue;
     removeButton.onclick = removeQueue;
 
-
     function addQueue() {
         if (item.value == '') {
-            console.error('XD');
+            console.error('Error, cadena vacía');
         } else {
-            var element2 = document.createElement("div");
-            element2.appendChild(document.createTextNode(`${item.value}`))
-            document.getElementById('table').appendChild(element2);
-        }
-    }
+            var el = document.createElement("div");
+            el.appendChild(document.createTextNode(`${item.value}`))
+            document.getElementById('table').appendChild(el);
+    }}
 
     function removeQueue() {
-        console.log('Working');
-        if ($('.table:empty').is(':empty')) {
-            console.error('XD');
+        if (document.getElementById('table').innerHTML === "") {
+            console.log('Error, tabla vacía');
+        } else {
+            var table = document.getElementById('table');
+            table.removeChild(table.firstElementChild);
         }
     }
 })
